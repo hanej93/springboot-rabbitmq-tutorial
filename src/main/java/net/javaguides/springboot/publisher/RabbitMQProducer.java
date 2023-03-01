3f499b2221d37dd1da441ae90fcac15cfeb04f76
EJ <hanej93@naver.com>
2023-03-01 12:01:49 +0900

    create rabbitmq producer

---
+++ src/main/java/net/javaguides/springboot/publisher/RabbitMQProducer.java
@@ -0,0 +1,27 @@
+package net.javaguides.springboot.publisher;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class RabbitMQProducer {
+
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public void sendMessage(String message) {
+        log.info(String.format("Message sent -> %s", message));
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+    }
+}
